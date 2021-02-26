82b5b1035aa2f0cb06bd0f319b603f220f14139f
Vitalii Marenkov <admin@mavik.com.ua>
2021-02-26 21:31:04 +0100

    Add tests for Gmagick::crop()

--- tests/src/GraphicLibrary/GmagickTest.php
+++ tests/src/GraphicLibrary/GmagickTest.php
@@ -47,6 +47,26 @@ class GmagickTest extends TestCase
         unlink($savedFile);
     }
 
+    public function testCrop()
+    {
+        $src = __DIR__ . '/../../resources/images/apple.jpg';
+        $savedFile = __DIR__ . '/../../temp/' . basename($src);
+        $expectedFile = __DIR__ . '/../../resources/images/apple-crop-25-40-800-900.jpg';
+        
+        $gmagick = new Gmagick();
+        $resource = $gmagick->open($src, IMAGETYPE_JPEG);
+        $resource = $gmagick->crop($resource, 25, 40, 800, 900);
+        $gmagick->save($resource, $savedFile, IMAGETYPE_JPEG);
+        
+        $imageSize = getimagesize($savedFile);
+        $this->assertEquals(800, $imageSize[0]);
+        $this->assertEquals(900, $imageSize[1]);
+        $this->assertEquals(IMAGETYPE_JPEG, $imageSize[2]);
+        
+        $this->assertLessThan(1, CompareImages::distance($expectedFile, $savedFile));
+        unlink($savedFile);        
+    }
+    
     public function imagesToOpen()
     {
         return [
