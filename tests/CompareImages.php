48e2ade874eb07cfc28e0973d48e0f46c078eb88
Vitalii Marenkov <admin@mavik.com.ua>
2021-03-24 22:08:50 +0100

    Use real pathes in CompareImages::distance

--- tests/CompareImages.php
+++ tests/CompareImages.php
@@ -14,13 +14,16 @@ class CompareImages
 {
     public static function distance(string $image1, string $image2): int
     {
-        exec("gm compare -metric MSE \"{$image1}\" \"{$image2}\"", $output);
+        $image1 = realpath($image1);
+        $image2 = realpath($image2);
+        $cmd = "gm compare -metric MSE \"{$image1}\" \"{$image2}\"";
+        exec($cmd, $output);
         foreach ($output as $line) {
             preg_match('/Total:\s+(0\.\d+)\s+\d+\.\d+/', $line, $matches);
             if (!empty($matches)) {
                 return round($matches[1] * 100);
             }
         }
-        throw new \Exception("Images '{$image1}' and '{$image2}' cannot be compared.");
+        throw new \Exception("Images '{$image1}' and '{$image2}' cannot be compared.\nCommand: {$cmd}");
     }
 }
