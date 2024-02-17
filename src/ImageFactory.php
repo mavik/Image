fac9881717bb1830761fc6e845a157d1e114d7f2
Vitalii Marenkov <admin@mavik.com.ua>
2024-02-17 17:15:10 +0100

    Add class ImageFactory

---
+++ src/ImageFactory.php
@@ -0,0 +1,43 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * PHP Library for Image processing and creating thumbnails
+ *
+ * @package Mavik\Image
+ * @author Vitalii Marenkov <admin@mavik.com.ua>
+ * @copyright 2021 Vitalii Marenkov
+ * @license MIT; see LICENSE
+ */
+namespace Mavik\Image;
+
+class ImageFactory
+{
+    /** @var Configuration */
+    private $configuration;
+
+    public function __construct(Configuration $configuration)
+    {
+        $this->configuration = $configuration;
+    }
+
+    public function create(string $src): Image
+    {
+        return Image::create($src, $this->configuration);
+    }
+    
+    public function createFromString(string $src): Image
+    {
+        return Image::createFromString($src, $this->configuration);
+    }
+    
+    public function createImmutable(string $src): ImageImmutable
+    {
+        return ImageImmutable::create($src, $this->configuration);
+    }
+    
+    public function createImmutableFromString(string $src): ImageImmutable
+    {
+        return ImageImmutable::createFromString($src, $this->configuration);
+    }
+}
